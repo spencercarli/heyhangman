baeb0b23cb1e048f3920eccad2f149cf91816abf
Spencer Carli <spencercarli@gmail.com>
2013-09-23 13:15:32 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Heyhangman::Application.config.secret_key_base = '9622a4462ce5ebd6fe05981713b249cf7af5ba6527bc8dfb8e4968674f9daeb435487430c420cb175fac64a55fc6003967c8754509dd24ad524214f360410f89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Heyhangman::Application.config.session_store :cookie_store, key: '_heyhangman_session'
