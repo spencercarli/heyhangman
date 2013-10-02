edc5e0b168054b7a1f17f32f1ecae68c74727205
Spencer Carli <spencercarli@gmail.com>
2013-10-02 13:28:31 -0400

    Access @word via javascript

---
+++ app/assets/javascripts/hangmen.js
@@ -0,0 +1,11 @@
+function main(word){
+	alert(word);
+}
+
+function readVal() {
+	var guess = document.getElementById('user-guess').value
+	var word = document.getElementById
+	
+	alert("User guess: " + guess);
+}
+
